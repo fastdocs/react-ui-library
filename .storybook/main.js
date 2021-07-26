b9f6b6eed51da1ecd709fd7260fa0ee1b5ed8caa
Daniel Luca <hey@danielluca.com>
2021-07-26 20:01:35 +0200

    feat: add viewport addon

--- .storybook/main.js
+++ .storybook/main.js
@@ -7,6 +7,7 @@ module.exports = {
     '@storybook/addon-controls',
     '@storybook/addon-actions',
     '@storybook/addon-a11y',
+    '@storybook/addon-viewport',
   ],
   webpackFinal: async (config) => {
     config.module.rules.push({
