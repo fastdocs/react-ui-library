a55f5637d91c488c3aa9243dbdeca3aa87492c06
Daniel Luca <hey@danielluca.com>
2021-07-26 19:33:27 +0200

    feat: add actions addon to storybook

--- .storybook/main.js
+++ .storybook/main.js
@@ -3,7 +3,11 @@ const path = require('path')
 module.exports = {
   stories: ['../src/**/*.stories.tsx'],
   // Add any Storybook addons you want here: https://storybook.js.org/addons/
-  addons: ['@storybook/addon-a11y', '@storybook/addon-controls'],
+  addons: [
+    '@storybook/addon-controls',
+    '@storybook/addon-actions',
+    '@storybook/addon-a11y',
+  ],
   webpackFinal: async (config) => {
     config.module.rules.push({
       test: /\.scss$/,
